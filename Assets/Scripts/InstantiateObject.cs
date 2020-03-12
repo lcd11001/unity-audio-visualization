40e8de1d3d6b2eb021a97a08c6f9a3fe3b72f1ed
Luong Cong Dan <lcd11001@gmail.com>
2020-03-12 11:53:37 +0700

    + cube spectrum

---
+++ Assets/Scripts/InstantiateObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    public GameObject _sampleObject;
+    public int _num;
+    public float _radius;
+    public float _maxScale;
+
+    GameObject[] _samples;
+
+    private void Awake()
+    {
+        _samples = new GameObject[_num];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < _num; i++)
+        {
+
+            GameObject cube = Instantiate(_sampleObject);
+            cube.transform.parent = this.transform;
+            cube.name = "SampleCube" + i;
+
+
+            this.transform.eulerAngles = new Vector3(0, -360.0f * i / _num, 0);
+            cube.transform.position = Vector3.forward * _radius;
+
+            // float angle = (-360.0f * i / _num) * Mathf.Deg2Rad;
+            // cube.transform.localPosition = new Vector3(Mathf.Sin(angle) * _radius, 0, Mathf.Cos(angle) * _radius);
+            // cube.transform.eulerAngles = new Vector3(0, angle * Mathf.Rad2Deg, 0);
+
+            _samples[i] = cube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < _num; i++)
+        {
+            if (_samples != null)
+            {
+                _samples[i].transform.localScale = new Vector3(1, (AudioPeer._samples[i] * _maxScale) + 2, 1);
+            }
+        }
+    }
+}
